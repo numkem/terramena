ecaca9ebab5bd0db9e5b11005132dc758a019357
Sebastien Bariteau <sebastien.bariteau@methodstudios.com>
2022-12-12 13:10:32 -0500

    update nixpkgs to latest (23.05)

--- flake.nix
+++ flake.nix
@@ -8,14 +8,19 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        rubyWithGems = pkgs.ruby_3_1.withPackages (ps: with ps; [ slop solargraph rspec rake ]);
-      in
-      {
+        devPackages = [
+          pkgs.rubyPackages_3_1.solargraph
+          pkgs.rubyPackages_3_1.rspec
+          pkgs.rubyPackages_3_1.rake
+        ];
+
+        rubyWithGems = pkgs.ruby_3_1.withPackages (ps: with ps; [ slop ]);
+      in {
         packages = flake-utils.lib.flattenTree {
           default = pkgs.stdenv.mkDerivation {
             name = "terramena";
             src = self;
-            buildInputs = with pkgs; [ rubyWithGems ];
+            buildInputs = [ rubyWithGems ];
 
             installPhase = ''
               mkdir -p $out/lib $out/bin $out/share
@@ -26,10 +31,7 @@
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rubyWithGems
-          ];
-        };
+        devShells.default =
+          pkgs.mkShell { buildInputs = [ rubyWithGems ] ++ devPackages; };
       });
 }
